ef4e981e8b27861f01be3f0b8ea297e8036f131e
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-06 15:15:09 +0200

    add: added MapData record to maps package, which represents a map with properties

---
+++ src/main/java/mazegame/map/MapData.java
@@ -0,0 +1,33 @@
+package mazegame.map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+public record MapData(@JsonProperty("id") int id, @JsonProperty("name") String name, @JsonProperty("rows") int rows,
+                      @JsonProperty("cols") int cols, @JsonProperty("blocks") List<List<Block>> blocks) {
+
+    public MapData(int id, String name, int rows, int cols, List<List<Block>> blocks) {
+        if (rows <= 0 || cols <= 0) {
+            throw new IllegalArgumentException("The number of rows and columns must be positive");
+        }
+
+        if (blocks.size() != rows) {
+            throw new IllegalArgumentException("The number of rows must match the number of blocks");
+        }
+
+        for (var row : blocks) {
+            if (row.size() != cols) {
+                throw new IllegalArgumentException("The number of columns must match the number of blocks");
+            }
+        }
+
+        this.id = id;
+        this.name = name;
+        this.rows = rows;
+        this.cols = cols;
+        this.blocks = blocks;
+    }
+
+}
