e5686c549d7b3d438baae56cf48509c3f99fc347
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-06 15:14:17 +0200

    add: added Block class to maps package, which represents a block with properties

---
+++ src/main/java/mazegame/map/Block.java
@@ -0,0 +1,56 @@
+package mazegame.map;
+
+public record Block(boolean wallTop, boolean wallRight, boolean wallBottom, boolean wallLeft,
+                    boolean start, boolean end, boolean monster) {
+
+
+    public Block of() {
+        return new Block(false, false, false, false, false, false, false);
+    }
+
+    public Block of(boolean wallTop) {
+        return new Block(wallTop, false, false, false, false, false, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight) {
+        return new Block(wallTop, wallRight, false, false, false, false, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight, boolean wallBottom) {
+        return new Block(wallTop, wallRight, wallBottom, false, false, false, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight, boolean wallBottom, boolean wallLeft) {
+        return new Block(wallTop, wallRight, wallBottom, wallLeft, false, false, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight, boolean wallBottom, boolean wallLeft, boolean start) {
+        return new Block(wallTop, wallRight, wallBottom, wallLeft, start, false, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight, boolean wallBottom, boolean wallLeft,
+                    boolean start, boolean end) {
+        if (start && end) {
+            throw new IllegalArgumentException("A block cannot be both a start and an end");
+        }
+
+        return new Block(wallTop, wallRight, wallBottom, wallLeft, start, end, false);
+    }
+
+    public Block of(boolean wallTop, boolean wallRight, boolean wallBottom, boolean wallLeft,
+                    boolean start, boolean end, boolean monster) {
+        if (start && end || start && monster || end && monster) {
+            throw new IllegalArgumentException("A block cannot be both a start and and end or a monster");
+        }
+
+        return new Block(wallTop, wallRight, wallBottom, wallLeft, start, end, monster);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Block{wallTop=%s, wallRight=%s, wallBottom=%s, wallLeft=%s," +
+                             "start=%s, end=%s, monster=%s}",
+                wallTop, wallRight, wallBottom, wallLeft, start, end, monster);
+    }
+
+}
