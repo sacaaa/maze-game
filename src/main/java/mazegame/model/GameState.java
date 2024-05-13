6157864f74587708a1cbc1a63dac8cc7e6a9e778
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-13 09:09:15 +0200

    edit: new methods for BreadthFirstSearch algorithm and console game

--- src/main/java/mazegame/model/GameState.java
+++ src/main/java/mazegame/model/GameState.java
@@ -2,34 +2,63 @@ package mazegame.model;
 
 import lombok.Getter;
 import mazegame.map.Block;
-import mazegame.map.MapData;
 import mazegame.map.Maps;
 import puzzle.State;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.Set;
 
+/**
+ * Represents the state of the game.
+ */
 @Getter
 public class GameState implements State<Direction> {
 
+    /**
+     * The maps of the game.
+     */
     private final Maps maps;
 
+    /**
+     * The index of the current map.
+     */
     private final int mapIndex = 1;
 
+    /**
+     * The positions of the player, monster, and end.
+     */
     private Position[] positions;
 
+    /**
+     * The index of the player in the positions array.
+     */
     public static final int PLAYER = 0;
 
+    /**
+     * The index of the monster in the positions array.
+     */
     public static final int MONSTER = 1;
 
+    /**
+     * The index of the end in the positions array.
+     */
     public static final int END = 2;
 
+    /**
+     * Constructs a new GameState with the specified maps.
+     *
+     * @param maps the maps of the game
+     */
     public GameState(Maps maps) {
         this.maps = maps;
         this.positions = new Position[3];
         init();
     }
 
+    /**
+     * Initializes the positions of the player, monster, and end.
+     */
     private void init() {
         var map = maps.getMap(mapIndex);
 
@@ -52,9 +81,31 @@ public class GameState implements State<Direction> {
         return positions[index];
     }
 
+    public void printState() {
+        var map = maps.getMap(mapIndex);
+
+        for (int i = 0; i < map.rows(); i++) {
+            for (int j = 0; j < map.cols(); j++) {
+                var position = new Position(i, j);
+
+                if (position.equals(getPosition(PLAYER))) {
+                    System.out.print("P ");
+                } else if (position.equals(getPosition(MONSTER))) {
+                    System.out.print("M ");
+                } else if (position.equals(getPosition(END))) {
+                    System.out.print("E ");
+                } else {
+                    System.out.print(map.blocks().get(i).get(j) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     @Override
     public boolean isSolved() {
-        return getPosition(PLAYER).equals(getPosition(END));
+        return getPosition(PLAYER).equals(getPosition(END))
+                || getPosition(PLAYER).equals(getPosition(MONSTER));
     }
 
     @Override
@@ -72,7 +123,7 @@ public class GameState implements State<Direction> {
     }
 
     private boolean canMoveUp(Block playerBlock) {
-        if (playerBlock.wallTop()) {
+        if (getPosition(PLAYER).row() == 0 || playerBlock.wallTop()) {
             return false;
         }
 
@@ -84,7 +135,7 @@ public class GameState implements State<Direction> {
     }
 
     private boolean canMoveRight(Block playerBlock) {
-        if (playerBlock.wallRight()) {
+        if  (getPosition(PLAYER).col() == maps.getMap(mapIndex).cols() - 1 || playerBlock.wallRight()) {
             return false;
         }
 
@@ -96,7 +147,7 @@ public class GameState implements State<Direction> {
     }
 
     private boolean canMoveDown(Block playerBlock) {
-        if (playerBlock.wallBottom()) {
+        if (getPosition(PLAYER).row() == maps.getMap(mapIndex).rows() - 1 || playerBlock.wallBottom()) {
             return false;
         }
 
@@ -108,7 +159,7 @@ public class GameState implements State<Direction> {
     }
 
     private boolean canMoveLeft(Block playerBlock) {
-        if (playerBlock.wallLeft()) {
+        if (getPosition(PLAYER).col() == 0 || playerBlock.wallLeft()) {
             return false;
         }
 
@@ -121,6 +172,9 @@ public class GameState implements State<Direction> {
 
     @Override
     public void makeMove(Direction direction) {
+        if (isLegalMove(direction)) {
+            positions[PLAYER] = getPosition(PLAYER).move(direction);
+        }
     }
 
     @Override
@@ -142,6 +196,20 @@ public class GameState implements State<Direction> {
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
         }
+        copy.positions = positions.clone();
         return copy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        return (o instanceof GameState other) && Arrays.equals(positions, other.positions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(positions);
     }
 }
