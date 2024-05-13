ca124604524729bb2f65fd51e79fb0d4966566f7
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-13 09:06:46 +0200

    edit: added equals method

--- src/main/java/mazegame/model/Position.java
+++ src/main/java/mazegame/model/Position.java
@@ -56,4 +56,11 @@ public record Position(int row, int col) {
         return String.format("(%d,%d)", row, col);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row && col == position.col;
+    }
 }
