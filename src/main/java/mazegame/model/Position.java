9d9e1f50a3d153919242cdfd46c4d336caf8d90b
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-28 14:25:39 +0200

    edit: deleted equals unused function

--- src/main/java/mazegame/model/Position.java
+++ src/main/java/mazegame/model/Position.java
@@ -56,11 +56,4 @@ public record Position(int row, int col) {
         return String.format("(%d,%d)", row, col);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Position position = (Position) o;
-        return row == position.row && col == position.col;
-    }
 }
