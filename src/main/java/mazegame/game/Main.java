7a250f834fa42ff76322065418a914c191e5621e
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-29 22:07:55 +0200

    edit: new main application launch

--- src/main/java/mazegame/game/Main.java
+++ src/main/java/mazegame/game/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) {
-        Application.launch(GameApplication.class, args);
+        Application.launch(MainApplication.class, args);
     }
 
 }
