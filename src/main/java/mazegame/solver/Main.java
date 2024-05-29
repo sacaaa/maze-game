eb062e957deef3b8ee9d2d306fcab1c47153cf17
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-05-29 10:08:12 +0200

    edit: manual solving and debugging

--- src/main/java/mazegame/solver/Main.java
+++ src/main/java/mazegame/solver/Main.java
@@ -5,6 +5,8 @@ import mazegame.model.Direction;
 import mazegame.model.GameState;
 import puzzle.solver.BreadthFirstSearch;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,8 +15,20 @@ public class Main {
         var maps = Maps.getInstance();
 
         // Solve the game
-        var bfs = new BreadthFirstSearch<Direction>();
-        bfs.solveAndPrintSolution(new GameState(maps, 1));
+//        var bfs = new BreadthFirstSearch<Direction>();
+//        bfs.solveAndPrintSolution(new GameState(maps, 1));
+
+        var gameState = new GameState(maps, 1);
+        Scanner scanner = new Scanner(System.in);
+        while (!gameState.isSolved()) {
+            try {
+                var direction = Direction.valueOf(scanner.nextLine());
+                gameState.makeMove(direction);
+            } catch (Exception e) {
+                // Ignore invalid input
+            }
+        }
+
     }
 
 }
