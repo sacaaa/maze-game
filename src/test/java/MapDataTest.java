63021c8f4f16cb4a5560bd0b062703535df7d443
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-05-31 15:50:55 +0200

    add: new test cases for the mapdata class

---
+++ src/test/java/MapDataTest.java
@@ -0,0 +1,46 @@
+import mazegame.map.Block;
+import mazegame.map.MapData;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class MapDataTest {
+
+   @Test
+    void success_MapData() {
+        var id = 1;
+        var name = "Map 1";
+        var rows = 3;
+        var cols = 3;
+        var blocks = List.of(
+                List.of(Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false)),
+                List.of(Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false)),
+                List.of(Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false),
+                        Block.of(true, false, false, true, false, false, false))
+        );
+
+        MapData mapData = new MapData(id, name, rows, cols, blocks);
+
+        assertEquals(id, mapData.id());
+        assertEquals(name, mapData.name());
+        assertEquals(rows, mapData.rows());
+        assertEquals(cols, mapData.cols());
+        assertEquals(blocks, mapData.blocks());
+    }
+
+    @Test
+    void failure_MapData() {
+        assertThrows(IllegalArgumentException.class, () -> new MapData(1, "Map 1", 0, 0, List.of()));
+        assertThrows(IllegalArgumentException.class, () -> new MapData(1, "Map 1", 3, 3, List.of()));
+    }
+
+}
