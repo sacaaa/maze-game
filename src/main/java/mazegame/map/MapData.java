d11ed07355a1a103bd590c57ac155f92cef04af4
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-06-01 10:59:05 +0200

    edit: missing javadoc

--- src/main/java/mazegame/map/MapData.java
+++ src/main/java/mazegame/map/MapData.java
@@ -4,9 +4,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+/**
+ * Represents the data of a map in the game.
+ *
+ * @param id the id of the map
+ * @param name the name of the map
+ * @param rows the number of rows in the map
+ * @param cols the number of columns in the map
+ * @param blocks the blocks of the map
+ */
 public record MapData(@JsonProperty("id") int id, @JsonProperty("name") String name, @JsonProperty("rows") int rows,
                       @JsonProperty("cols") int cols, @JsonProperty("blocks") List<List<Block>> blocks) {
 
+    /**
+     * Constructor for creating a new MapData object.
+     *
+     * @param id the id of the map
+     * @param name the name of the map
+     * @param rows the number of rows in the map
+     * @param cols the number of columns in the map
+     * @param blocks the blocks of the map
+     * @throws IllegalArgumentException if the number of rows or columns is not positive, or the number of rows or columns does not match the number of blocks
+     */
     public MapData(int id, String name, int rows, int cols, List<List<Block>> blocks) {
         if (rows <= 0 || cols <= 0) {
             throw new IllegalArgumentException("The number of rows and columns must be positive");
