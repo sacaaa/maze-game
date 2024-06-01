bfbbb573a587cb05ca270bb3cefcce1802622b22
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-06-01 11:04:45 +0200

    edit: removed unused manual solving section

--- src/main/java/mazegame/solver/Main.java
+++ src/main/java/mazegame/solver/Main.java
@@ -3,8 +3,7 @@ package mazegame.solver;
 import mazegame.map.Maps;
 import mazegame.model.Direction;
 import mazegame.model.GameState;
-
-import java.util.Scanner;
+import puzzle.solver.BreadthFirstSearch;
 
 public class Main {
 
@@ -14,20 +13,8 @@ public class Main {
         var maps = Maps.getInstance();
 
         // Solve the game
-//        var bfs = new BreadthFirstSearch<Direction>();
-//        bfs.solveAndPrintSolution(new GameState(maps, 1));
-
-        var gameState = new GameState(maps, 1);
-        Scanner scanner = new Scanner(System.in);
-        while (!gameState.isSolved()) {
-            try {
-                var direction = Direction.valueOf(scanner.nextLine());
-                gameState.makeMove(direction);
-            } catch (Exception e) {
-                // Ignore invalid input
-            }
-        }
-
+        var bfs = new BreadthFirstSearch<Direction>();
+        bfs.solveAndPrintSolution(new GameState(maps, 1));
     }
 
 }
